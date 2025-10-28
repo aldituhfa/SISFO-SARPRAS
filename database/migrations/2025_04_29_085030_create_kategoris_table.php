ade211bdd98bf0a26ec9aa0508d9c763c7b12add
aldituhfa <aldituhfa17@gmail.com>
2025-10-28 14:23:51 +0700

    sarpras new

--- database/migrations/2025_04_29_085030_create_kategoris_table.php
+++ database/migrations/2025_04_29_085030_create_kategoris_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('kategoris', function (Blueprint $table) {
-            $table->unsignedBigInteger('id')->primary(); // id manual
+            $table->id();
             $table->string('nama_kategori');
             $table->timestamps();
         });
